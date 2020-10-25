6c29259673e4cdeee8e388cd32153eb53e78a888
Roger D. Winans <solvaholic@users.noreply.github.com>
2020-10-25 19:48:38 -0400

    007

--- repo_config.sh
+++ repo_config.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # https://docs.github.com/en/free-pro-team@latest/actions/reference/environment-variables
 
@@ -12,14 +12,14 @@ _file=".github/repo_config.json"
 
 if [ -r "${_file}" ]; then
 
-  jq .settings "${_file}" | curl -d @- \
+  jq .settings "${_file}" | curl -v -d @- \
     -X PATCH \
     -H "Accept: application/vnd.github.v3+json" \
     -H "Content-Type: application/json" \
     -H "Authorization: token ${_token}" \
     "${_url}/repos/${_repo}"
 
-  jq .branch_protection "${_file}" | curl -d @- \
+  jq .branch_protection "${_file}" | curl -v -d @- \
     -X PUT \
     -H "Accept: application/vnd.github.luke-cage-preview+json" \
     -H "Content-Type: application/json" \

009a713407ec0252a4a78b313ece0f63bc9e92f1
Roger D. Winans <solvaholic@users.noreply.github.com>
2020-10-25 20:10:36 -0400

    009

--- repo_config.sh
+++ repo_config.sh
@@ -2,24 +2,24 @@
 
 # https://docs.github.com/en/free-pro-team@latest/actions/reference/environment-variables
 
-echo "INPUT_DEFAULT-BRANCH is '${INPUT_DEFAULT-BRANCH}'"
+echo "INPUT_CONFIG_FILE is '${INPUT_CONFIG_FILE}'"
 
-_token="${INPUT_GITHUB-TOKEN}"
+_token="${GITHUB_TOKEN}"
 _repo="${GITHUB_REPOSITORY}"
-_branch="${INPUT_DEFAULT-BRANCH}"
+_branch="${DEFAULT_BRANCH:-main}"
 _url="${GITHUB_API_URL}"
-_file=".github/repo_config.json"
+_file="${INPUT_CONFIG_FILE}"
 
 if [ -r "${_file}" ]; then
 
-  jq .settings "${_file}" | curl -v -d @- \
+  jq .settings "${_file}" | curl -i -d @- \
     -X PATCH \
     -H "Accept: application/vnd.github.v3+json" \
     -H "Content-Type: application/json" \
     -H "Authorization: token ${_token}" \
     "${_url}/repos/${_repo}"
 
-  jq .branch_protection "${_file}" | curl -v -d @- \
+  jq .branch_protection "${_file}" | curl -i -d @- \
     -X PUT \
     -H "Accept: application/vnd.github.luke-cage-preview+json" \
     -H "Content-Type: application/json" \
